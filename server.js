733312e1c99ea06e438f36e42c44a66e27b16c21
UR1232 <UR1232@users.noreply.github.com>
2026-09-15 05:36:10 +0300

    chore: update APK_DOWNLOAD_URL to v1.0.38 direct Catbox link

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const REVOKED_FILE = path.join(__dirname, 'revoked_sessions.json');
 
 const APP_VERSION = '1.0.38';
 const APP_VERSION_CODE = 38;
-let APK_DOWNLOAD_URL = 'https://files.catbox.moe/qwwcrf.apk';
+let APK_DOWNLOAD_URL = 'https://files.catbox.moe/ovatdw.apk';
 
 // دالة تحويل الأرقام العربية والفارسية إلى أرقام إنجليزية قياسية
 function normalizeDigits(str) {
