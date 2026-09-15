5ce1978c3221714569733c374af9546dc5df1dae
UR1232 <UR1232@users.noreply.github.com>
2026-09-15 06:19:56 +0300

    feat(v1.0.41): sleek rectangular circular title bar and detail popup modal

--- server.js
+++ server.js
@@ -27,9 +27,9 @@ const ANNOUNCEMENT_FILE = path.join(__dirname, 'announcement.json');
 const ANNOUNCEMENTS_LIST_FILE = path.join(__dirname, 'announcements.json');
 const REVOKED_FILE = path.join(__dirname, 'revoked_sessions.json');
 
-const APP_VERSION = '1.0.40';
-const APP_VERSION_CODE = 40;
-let APK_DOWNLOAD_URL = 'https://files.catbox.moe/ltpe7l.apk';
+const APP_VERSION = '1.0.41';
+const APP_VERSION_CODE = 41;
+let APK_DOWNLOAD_URL = 'https://files.catbox.moe/eewxel.apk';
 
 // دالة تحويل الأرقام العربية والفارسية إلى أرقام إنجليزية قياسية
 function normalizeDigits(str) {
