580a66dbb7bb2f5672bd4d0d036f9374e5c3728f
UR1232 <UR1232@users.noreply.github.com>
2026-09-15 04:47:14 +0300

    feat: release official signed production APK v1.0.35 with custom developer certificate

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const REVOKED_FILE = path.join(__dirname, 'revoked_sessions.json');
 
 const APP_VERSION = '1.0.35';
 const APP_VERSION_CODE = 35;
-let APK_DOWNLOAD_URL = 'https://files.catbox.moe/qi0ayo.apk';
+let APK_DOWNLOAD_URL = 'https://files.catbox.moe/thqwg8.apk';
 
 // دالة تحويل الأرقام العربية والفارسية إلى أرقام إنجليزية قياسية
 function normalizeDigits(str) {
